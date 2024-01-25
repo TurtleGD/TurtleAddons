05c582524c443acbaa0b14bf9dee649f6efb7ce7
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-01-25 15:02:14 -0500

    Add files via upload

---
+++ features/discord.js
@@ -0,0 +1,32 @@
+import settings from "../settings";
+import { request } from "../../axios";
+
+function sendWebhook(message) {
+    let webhookMessage = '';
+    webhookMessage = settings.webhookMessage;
+
+    if (settings.webhookMessage == 'chat') webhookMessage = message;
+    if (settings.webhookCoords == true) webhookMessage += ` | x: ${Math.round(Player.getX())}, y: ${Math.round(Player.getY())}, z: ${Math.round(Player.getZ())}`;
+    webhookMessage += ` ${settings.webhookPing}`
+
+    request({
+        url: settings.webhookLink,
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "User-Agent": "Mozilla/5.0"
+        },
+        body: {
+            "username": settings.webhookName,
+            "avatar_url": settings.webhookPfp,
+            "content": webhookMessage
+        }
+    });
+}
+
+register("chat", (message) => {
+    if (settings.discordWebhook == true && message.includes(settings.webhookMatch)) {
+        if (settings.webhookNonPlayer == true && message.includes(':')) return;
+        else sendWebhook(message);
+    }
+}).setCriteria("${message}");
