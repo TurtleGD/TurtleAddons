6020a86c447b72bb1636eb41aee73f6acc1f8ca2
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-02-02 20:04:50 -0500

    Update kuudra.js

--- features/kuudra.js
+++ features/kuudra.js
@@ -1,6 +1,6 @@
 import settings from "../settings";
-import { isPlayerAt, createWaypoint, nearCoords } from "../exports";
-import { getMatchFromLines, getScoreboard, removeUnicode } from "../../BloomCore/utils/Utils";
+import { isPlayerAt, createWaypoint, nearCoords, inTrueLair } from "../exports";
+import { RED, BOLD } from "../exports";
 
 var startTime;
 var endTime;
@@ -24,17 +24,35 @@ register('renderWorld', () => {
     if (settings.stunBlock && nearCoords(-153, 31, -172, 40)) createWaypoint(-153, 31, -172, 0, 0, 255, 0.25, 1, true);
 });
 
+// Party DPS
 register("chat", (message) => {
-    if (settings.partyDps && message.includes("KUUDRA DOWN!") && !message.includes(':') && removeUnicode(getMatchFromLines(/ ⏣ (.+)/, getScoreboard(false))) == "Kuudra's Hollow (T5)") {
+    if (!settings.partyDps) return;
+    
+    if (message.includes("KUUDRA DOWN!") && !message.includes(':') && inTrueLair()) {
         endTime = new Date().getTime();
         timeToKill = (endTime - startTime) / 1000;
-        ChatLib.command(`pc Party True DPS: ${(300/timeToKill).toFixed(2)}m`);
+        ChatLib.command(`pc Party DPS: ${(300/timeToKill).toFixed(2)}m`);
     };
 }).setCriteria("${message}");
 
 register('tick', () => {
-    if ((settings.partyDps && -87 > Player.getX() && Player.getX() > -118 && 32 > Player.getY() && Player.getY() > 3 && -94 > Player.getZ() && Player.getZ() > -120) && !counting) {
+    if (!settings.partyDps) return;
+
+    if (inTrueLair() && !counting) {
         startTime = new Date().getTime();
         counting = true;
     };
+});
+
+
+// Energized Chunk Alert
+register("tick", () => {
+    if (settings.chunkAlert) return;
+
+    World.getAllEntitiesOfType(Java.type('net.minecraft.entity.item.EntityArmorStand').class).forEach(stand => {
+        if (inTrueLair() && stand.getName().removeFormatting().toLowerCase().includes('energized chunk') && nearCoords(stand.getX(), stand.getY(), stand.getZ()) < 8) {
+            World.playSound('note.pling', 0.1, 2);
+            Client.showTitle(`${RED + BOLD}CHUNK!`, '', 0, 2, 0);
+        };
+    });
 });
