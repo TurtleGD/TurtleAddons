b6716005dc85ac3b51327deff570b1a761eddb73
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-02-19 01:59:28 -0500

    Update slayers.js

--- features/slayers.js
+++ features/slayers.js
@@ -1,6 +1,9 @@
 import settings from "../settings";
-import { LIGHT_PURPLE, rune } from "../exports";
+import { LIGHT_PURPLE, RED, rune, pling, AQUA, GRAY, WHITE, GREEN } from "../exports";
 
+let gummyTimeLeft;
+
+// Rare drops
 register("chat", (message) => {
     if (message.includes(':') || !message.includes('DROP!')) return;
 
@@ -38,13 +41,33 @@ register("chat", (message) => {
 
         if (settings.slayerDropTitle) {
             Client.showTitle(`${LIGHT_PURPLE + dropTitle}`, '', 0, 40, 20);
-            for (let i = 0; i < 3 ;i++) {
-                (function (index) {
-                    setTimeout(() => {
-                        World.playSound('note.pling', 1.0, 2.0);
-                    }, 75 * index);
-                })(i);
-            };
+            pling.play();
         };
     };
+}).setCriteria("${message}");
+
+
+// Smoldering polarization warning
+register('step', () => {
+    if (!settings.gummyWarning) return;
+    
+    if (ChatLib.removeFormatting(TabList.getFooter()).includes(`Smoldering Polarization I ${settings.gummyTimer}m`) || gummyTimeLeft == settings.gummyTimer) {
+        Client.showTitle(`${RED}GUMMY LOW!`, '', 0, 60, 20);
+        pling.play();
+    }
+    gummyTimeLeft -= 1;
+}).setDelay(60);
+
+register('command', (arg) => {
+    if (!isNaN(parseInt(arg))) {
+        ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Set ${GREEN}Smoldering Polarization I${WHITE} to ${arg}m.`)
+        gummyTimeLeft = arg;
+    } else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument, enter an integer.`)
+}).setName('gummy')
+
+
+// Hide attunements
+register('chat', (message, event) => {
+    if (!settings.hideAttunements) return;
+    if (message.startsWith("Strike using the") || message == 'Your hit was reduced by Hellion Shield!') cancel(event)
 }).setCriteria("${message}");
