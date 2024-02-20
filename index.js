f5c269df2b8caa4dc89a68e03a7a998877e9703c
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-02-19 19:12:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,19 +14,18 @@ register('command', (arg) => {
         ChatLib.chat('');
         ChatLib.chat(`${AQUA + BOLD}/turtleaddons, /ta, /turtle, /8joh, /joh ${RESET + WHITE}- Open settings.`);
         ChatLib.chat(`${AQUA + BOLD}/getnbt ${RESET + WHITE}- Send NBT data of held item into chat. Open '/ct console' to get color codes.`);
-        ChatLib.chat(`${AQUA + BOLD}/gummy [minutes] ${RESET + WHITE}- Set smoldering polarization timer. See settings for more info.`);
-    }
+        ChatLib.chat(`${AQUA + BOLD}/gummy [minutes] ${RESET + WHITE}- Set smoldering polarization timer.`);
+        ChatLib.chat(`${AQUA + BOLD}/gummytime ${RESET + WHITE}- See remaining time of smoldering polarization.`);
+    }    ;
+    if (arg == 'changelog') {
+        axios.get('https://chattriggers.com/api/modules/1882').then(response => {if (response.data.releases[0].releaseVersion != JSON.parse(FileLib.read("TurtleAddons", "metadata.json")).version){
+        ChatLib.chat(`${BOLD + UNDERLINE}Changelog:`);
+        ChatLib.chat('');
+        ChatLib.chat(response.data.releases[0].changelog);
+        }});
+    };
 }).setName('turtleaddons').setAliases('ta', 'turtle', '8joh', 'joh');
 
 register('command', () => {
     ChatLib.simulateChat(Player.getHeldItem()?.getNBT());
 }).setName('getnbt');
-
-// Update message
-axios.get('https://chattriggers.com/api/modules/1882').then(response => {if (response.data.releases[0].releaseVersion != JSON.parse(FileLib.read("TurtleAddons", "metadata.json")).version){
-    new TextComponent(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Version ${response.data.releases[0].releaseVersion} Avaliable. (${AQUA}Click to Update!${GRAY})`).setClickAction("run_command").setClickValue(`/ct load`).chat()
-    ChatLib.chat('');
-    ChatLib.chat(`${BOLD + UNDERLINE}Changelog:`);
-    ChatLib.chat('');
-    ChatLib.chat(response.data.releases[0].changelog);
-}});
