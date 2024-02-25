95478df1317d6182685562c083a5e49f075024e9
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-02-25 16:37:06 -0500

    Update general.js

--- features/general.js
+++ features/general.js
@@ -2,7 +2,16 @@ import settings from "../settings";
 import { level, pling } from "../exports";
 
 let kickTime;
-let kicked = false
+let kicked = false;
+let kickedAndLobbied = false;
+
+register('worldLoad', () => {
+  if (kicked) {
+    kickTime = new Date().getTime();
+    kickedAndLobbied = true;
+    kicked = false;
+  }
+});
 
 // Level up sound effect
 register('chat', (message) => {
@@ -13,21 +22,18 @@ register('chat', (message) => {
 // Lobby timer
 register('chat', (message) => {
     if (message.includes(':') || !settings.kickedTimer) return;
-    if (message.includes('You were kicked while joining that server!') && !kicked) {
-        kickTime = new Date().getTime();
-        kicked = true;
-    }
+    if (message.includes('You were kicked while joining that server!') && !kicked) kicked = true;
 }).setCriteria("${message}");
 
 register("renderOverlay", () => {
-    if (kicked) {
+    if (kickedAndLobbied) {
       let timeLeft = new Date().getTime();
       timeLeft = 60 - (timeLeft - kickTime) / 1000;
       if (timeLeft >= 0) Renderer.drawString(`${timeLeft.toFixed(3)}`, Renderer.screen.getWidth() / 2 - 15, Renderer.screen.getHeight() / 2 + 6);
       if (timeLeft < 0) {
         kickTime = undefined;
-        kicked = false
-        pling.play()
+        kickedAndLobbied = false;
+        pling.play();
       }
     };
 });
