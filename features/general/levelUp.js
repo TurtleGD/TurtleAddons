fd2e753920695296732fd68b703cfa29cd9e20d2
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-03-30 01:59:32 -0400

    Update levelUp.js

--- features/general/levelUp.js
+++ features/general/levelUp.js
@@ -2,10 +2,10 @@ import settings from "../../settings";
 import { level } from "../../utils/sounds";
 
 register('chat', (message) => {
-if (settings.levelSound) {
-  if (!message.includes(':') && message.includes('LEVEL UP')) {
-    level.setVolume(settings.levelVolume / 100);
-    level.play();
+  if (settings.levelSound) {
+    if (!message.includes(':') && message.includes('LEVEL UP')) {
+      level.setVolume(settings.levelVolume / 100);
+      level.play();
+    };
   };
-};
 }).setCriteria("${message}");
