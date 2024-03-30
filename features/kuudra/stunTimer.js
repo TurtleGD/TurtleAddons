c90a93ec71fb80c7226ed1b060bb676b84c431af
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-03-30 02:01:52 -0400

    Update stunTimer.js

--- features/kuudra/stunTimer.js
+++ features/kuudra/stunTimer.js
@@ -10,7 +10,7 @@ register('worldLoad', () => {
 register("chat", (message) => {
     if (settings.stunTimer) {
         if (message == "[NPC] Elle: Phew! The Ballista is finally ready! It should be strong enough to tank Kuudra's blows now!") p3 = new Date().getTime();
-        if (message.includes("destroyed one of Kuudra's pods!") && !message.includes(':')) ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Stun took ${((new Date().getTime() - p3) / 1000).toFixed(3)}s.`);
-        if (settings.entryTimer && message == "[NPC] Elle: Oh, funny seeing you here.") ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Entry took ${((new Date().getTime() - p3) / 1000).toFixed(3)}s.`);
+        if (message.includes("destroyed one of Kuudra's pods!") && !message.includes(':')) ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Stun took ${((new Date().getTime() - p3) / 1000).toFixed(2)}s.`);
+        if (settings.entryTimer && message == "[NPC] Elle: Oh, funny seeing you here.") ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Entry took ${((new Date().getTime() - p3) / 1000).toFixed(2)}s.`);
     };
 }).setCriteria("${message}");
