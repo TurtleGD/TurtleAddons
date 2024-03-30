72dbcd6cc927ad570bff2b0bd386d21ea210a40d
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-03-30 01:55:10 -0400

    Update pogData.js

--- utils/pogData.js
+++ utils/pogData.js
@@ -12,15 +12,29 @@ export const pogData = new PogObject("TurtleAddons", {
     x1: [],
     x2: [],
 
+    blacklist: [],
+
     petXPX: 490,
     petXPY: 290,
+    petXPScale: 1,
 
     maskX: 490,
     maskY: 249,
+    maskScale: 1,
 
     ragX: 490,
     ragY: 249,
+    ragScale: 1,
 
     lobbyX: 490,
     lobbyY: 249,
+    lobbyScale: 1,
+
+    gummyX: 490,
+    gummyY: 249,
+    gummyScale: 1,
+
+    srbX: 490,
+    srbY: 249,
+    srbScale: 1,
 });
