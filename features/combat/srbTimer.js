7e3934bdf48d4ddb8c4639d8b2d36e69af780427
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-04-14 03:05:55 -0400

    Update srbTimer.js

--- features/combat/srbTimer.js
+++ features/combat/srbTimer.js
@@ -6,44 +6,41 @@ let showThing = false;
 let startTime = undefined;
 
 register('soundPlay', (pos, name, vol, pitch, cat, event) => {
-  if (settings.srbTimer) {
-    if (pitch == 1 && !startTime && Player.getHeldItem()?.getNBT()?.getCompoundTag("tag")?.getCompoundTag("ExtraAttributes")?.getString('id') == 'SOULS_REBOUND' && !Client.isInGui()) {
-      startTime = new Date().getTime();
+    if (settings.srbTimer) {
+        if (pitch == 1 && !startTime && Player.getHeldItem()?.getNBT()?.getCompoundTag("tag")?.getCompoundTag("ExtraAttributes")?.getString('id') == 'SOULS_REBOUND' && !Client.isInGui()) {
+            startTime = new Date().getTime();
+        }
     }
-  }
 }).setCriteria('random.successful_hit')
 
 register('renderOverlay', () => {
-  if (!showThing && startTime) {
-    if ((5 - ((new Date().getTime() - startTime) / 1000) > 0)) {
-      Renderer.scale(pogData.srbScale);
-      Renderer.drawString(`${AQUA}Souls Rebound: ${WHITE + (5 - ((new Date().getTime() - startTime) / 1000)).toFixed(3)}s`, pogData.srbX / pogData.srbScale, pogData.srbY / pogData.srbScale, true);
+    if (!showThing && startTime) {
+        if ((5 - ((new Date().getTime() - startTime) / 1000) > 0)) {
+            Renderer.scale(pogData.srbScale);
+            Renderer.drawString(`${AQUA}Souls Rebound: ${WHITE + (5 - ((new Date().getTime() - startTime) / 1000)).toFixed(3)}s`, pogData.srbX / pogData.srbScale, pogData.srbY / pogData.srbScale, true);
+        }
+        if (5 - ((new Date().getTime() - startTime) / 1000) <= 0) startTime = undefined;
+    } else if (showThing) {
+        Renderer.scale(pogData.srbScale);
+        Renderer.drawString(`${AQUA}Souls Rebound: ${WHITE}5.000s`, pogData.srbX / pogData.srbScale, pogData.srbY / pogData.srbScale, true);
     }
-    if (5 - ((new Date().getTime() - startTime) / 1000) <= 0) startTime = undefined;
-  } else if (showThing) {
-    Renderer.scale(pogData.srbScale);
-    Renderer.drawString(`${AQUA}Souls Rebound: ${WHITE}5.000s`, pogData.srbX / pogData.srbScale, pogData.srbY / pogData.srbScale, true);
-  }
 })
 
 register('command', (...args) => {
-  if (args) {
-    if (args[0].toLowerCase() == 'x') {
-      if (!isNaN(parseInt(args[1]))) pogData.srbX = parseInt(args[1]);
-      else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+    if (args) {
+        if (args[0].toLowerCase() == 'x') {
+            if (!isNaN(parseInt(args[1]))) pogData.srbX = parseInt(args[1]);
+            else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+        } else if (args[0].toLowerCase() == 'y') {
+            if (!isNaN(parseInt(args[1]))) pogData.srbY = parseInt(args[1]);
+            else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+        } else if (args[0].toLowerCase() == 'scale') {
+            if (!isNaN(parseInt(args[1]))) pogData.srbScale = parseInt(args[1]);
+            else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+        } else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use "x", "y", or "scale".`);
     }
-    else if (args[0].toLowerCase() == 'y') {
-      if (!isNaN(parseInt(args[1]))) pogData.srbY = parseInt(args[1]);
-      else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
-    }
-    else if (args[0].toLowerCase() == 'scale') {
-      if (!isNaN(parseInt(args[1]))) pogData.srbScale = parseInt(args[1]);
-      else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
-    }
-    else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use "x", "y", or "scale".`);
-  }
-  pogData.save();
+    pogData.save();
 
-  showThing = true;
-  setTimeout(() => showThing = false, 2000);
+    showThing = true;
+    setTimeout(() => showThing = false, 2000);
 }).setName('movesrb')
