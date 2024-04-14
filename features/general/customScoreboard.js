e4e00af991870a1d524b68afd7eebca6d541389e
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-04-14 02:39:39 -0400

    Add files via upload

---
+++ features/general/customScoreboard.js
@@ -0,0 +1,67 @@
+import settings from "../../settings";
+
+let widgets = settings.scoreboardWidgets.split(/\s*,\s*/);
+
+let customScoreboard = [];
+let width = 0;
+let text = new Text('');
+
+register('step', () => {
+    widgets = settings.scoreboardWidgets.split(/\s*,\s*/);
+
+    if (settings.customScoreboard) Scoreboard.setShouldRender(false);
+    else Scoreboard.setShouldRender(true);
+}).setDelay(1)
+
+
+register('tick', () => {
+    if (settings.customScoreboard) {
+        // Reset values
+        width = 0;
+        customScoreboard.length = 0;
+
+        // SKYBLOCK CO-OP, etc
+        let title = Scoreboard.getTitle();
+
+        // Add default scoreboard to list
+        customScoreboard = Scoreboard.getLines().slice().reverse().map(String);
+
+        // Add widgets if applicable
+        for (let i = 0; i < widgets.length; i++) {
+            TabList.getNames().forEach((name, j) => {
+                if (name.removeFormatting().startsWith(`${widgets[i]}:`)) {
+                    customScoreboard.splice(customScoreboard.length - 1, 0, TabList.getNames()[j]);
+                    let counter = 1;
+                    while (true) {
+                        if (!TabList.getNames()[j + counter] || TabList.getNames()[j + counter].removeFormatting() == '' || !TabList.getNames()[j + counter].removeFormatting().startsWith(' ')) break;
+                        else {
+                            customScoreboard.splice(customScoreboard.length - 1, 0, TabList.getNames()[j + counter]);
+                            counter += 1;
+                        }
+                    }
+                    customScoreboard.splice(customScoreboard.length - 1, 0, '');
+                }
+            })
+        }
+
+        // Adjusts scoreboard width
+        width = Math.max(width, ...customScoreboard.map(line => Renderer.getStringWidth(line) + 10))
+
+        // Centers title
+        while (Renderer.getStringWidth(title.removeFormatting()) < width - 15) {
+            title = ` ${title} `;
+        }
+        customScoreboard.unshift(title);
+
+        // Merges list
+        text.setString(customScoreboard.join('\n'));
+    }
+})
+
+
+register('renderOverlay', () => {
+    if (settings.customScoreboard) {
+        Renderer.drawRect((255 / (100 / settings.customScoreboardOpacity) << 24) | (0 << 16) | (0 << 8) | 0, Renderer.screen.getWidth() - width, (Renderer.screen.getHeight() / 2) - (text.getHeight() / 2), width, text.getHeight() + 2);
+        text.draw(Renderer.screen.getWidth() - width + 2, (Renderer.screen.getHeight() / 2) - (text.getHeight() / 2) + 2);
+    }
+})
