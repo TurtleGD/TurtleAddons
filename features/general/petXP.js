c11ca9eae94dcc3e31168eb2a3051fdc67d92c29
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-04-14 02:40:28 -0400

    Update petXP.js

--- features/general/petXP.js
+++ features/general/petXP.js
@@ -2,59 +2,88 @@ import settings from "../../settings";
 import { pogData } from "../../utils/pogData";
 import { AQUA, RESET, GRAY, WHITE } from "../../utils/formatting";
 
-let petXP = undefined;
-let showThing = false;
-let fishingPetXP = '';
+let petXPIndex = undefined;
+let showPetXP = false;
+let loadingThing = true;
+let noFishingPet = true
+let fishingPetXP = ''
 
 register('step', () => {
-  if (settings.petXPFishing.length > 0){
-    for (let i = 0; i < TabList.getNames().length; i++) {
-      if (TabList.getNames()[i].includes(settings.petXPFishing)) {
-        fishingPetXP = TabList.getNames()[i + 1];
-        break;
-      } 
+    loadingThing = false;
+
+    if (settings.petXPFishing.length > 0) {
+        for (let i = 0; i < TabList.getNames().length; i++) {
+            if (TabList.getNames()[i].includes(settings.petXPFishing) && noFishingPet) {
+                petXPIndex = i + 1;
+                noFishingPet = false;
+                fishingPetXP = TabList.getNames()[petXPIndex].removeFormatting()
+                break;
+            }
+        }
+        if (!TabList.getNames().join('').includes('Pet:')) petXPIndex = undefined;
+
+    } else if (settings.petXP) {
+        for (let i = 0; i < TabList.getNames().length; i++) {
+            if (TabList.getNames()[i].includes('Pet:')) {
+                petXPIndex = i + 2;
+                break;
+            }
+        }
+        if (!TabList.getNames().join('').includes('Pet:')) petXPIndex = undefined;
     }
-  } else if (settings.petXP){
-    for (let i = 0; i < TabList.getNames().length; i++) {
-      if ((TabList.getNames()[i]).removeFormatting().includes('XP (') || TabList.getNames()[i].includes('MAX LEVEL')) {
-        petXP = i;
-        break;
-      } 
-    }
-  }
 }).setDelay(1)
 
+register('worldLoad', () => {
+    loadingThing = true;
+    petXPIndex = undefined;
+})
+
 register('renderOverlay', () => {
-  if (petXP && !showThing && settings.petXPFishing.length == 0) {
-    Renderer.scale(pogData.petXPScale);
-    Renderer.drawString(`${AQUA}Pet XP:${RESET}${(TabList.getNames()[petXP])?.removeFormatting()}`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
-  } else if (!showThing && settings.petXPFishing.length > 0) {
-    Renderer.scale(pogData.petXPScale);
-    Renderer.drawString(`${AQUA}Pet XP (${settings.petXPFishing}):${RESET}${fishingPetXP?.removeFormatting()}`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
-  } else if (showThing) {
-    Renderer.scale(pogData.petXPScale);
-    Renderer.drawString(`${AQUA}Pet XP: ${RESET}0/123,456 (0%)`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
-  }
+    // Runs before the first check
+    if ((settings.petXPFishing.length > 0 || settings.petXP) && loadingThing && petXPIndex == undefined) {
+        Renderer.scale(pogData.petXPScale);
+        Renderer.drawString(`${AQUA}Pet XP:${RESET} Loading...`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
+
+    // If pet widget off with setting on
+    } else if (settings.petXPFishing.length > 0 && noFishingPet) {
+        Renderer.scale(pogData.petXPScale);
+        Renderer.drawString(`${AQUA}Pet XP:${RESET} Please equip your pet to level.`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
+    } else if ((settings.petXPFishing.length > 0 || settings.petXP) && petXPIndex == undefined) {
+        Renderer.scale(pogData.petXPScale);
+        Renderer.drawString(`${AQUA}Pet XP:${RESET} Please enable the Pet Widget.`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
+
+    // Fishing pet xp
+    } else if (!showPetXP && settings.petXPFishing.length > 0) {
+        Renderer.scale(pogData.petXPScale);
+        Renderer.drawString(`${AQUA}Pet XP (${settings.petXPFishing}):${RESET}${fishingPetXP}`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
+
+    // Current pet xp
+    } else if (!showPetXP && settings.petXP) {
+        Renderer.scale(pogData.petXPScale);
+        Renderer.drawString(`${AQUA}Pet XP:${RESET}${TabList.getNames()[petXPIndex].removeFormatting()}`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
+
+    // Example value when moving location
+    } else if (showPetXP) {
+        Renderer.scale(pogData.petXPScale);
+        Renderer.drawString(`${AQUA}Pet XP: ${RESET}0/123,456 (0%)`, pogData.petXPX / pogData.petXPScale, pogData.petXPY / pogData.petXPScale, true);
+    }
 })
 
 register('command', (...args) => {
-  if (args) {
-    if (args[0].toLowerCase() == 'x') {
-      if (!isNaN(parseInt(args[1]))) pogData.petXPX = parseInt(args[1]);
-      else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+    if (args) {
+        if (args[0].toLowerCase() == 'x') {
+            if (!isNaN(parseInt(args[1]))) pogData.petXPX = parseInt(args[1]);
+            else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+        } else if (args[0].toLowerCase() == 'y') {
+            if (!isNaN(parseInt(args[1]))) pogData.petXPY = parseInt(args[1]);
+            else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+        } else if (args[0].toLowerCase() == 'scale') {
+            if (!isNaN(parseInt(args[1]))) pogData.petXPScale = parseInt(args[1]);
+            else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
+        } else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use "x", "y", or "scale".`);
     }
-    else if (args[0].toLowerCase() == 'y') {
-      if (!isNaN(parseInt(args[1]))) pogData.petXPY = parseInt(args[1]);
-      else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
-    }
-    else if (args[0].toLowerCase() == 'scale') {
-      if (!isNaN(parseInt(args[1]))) pogData.petXPScale = parseInt(args[1]);
-      else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use a number.`);
-    }
-    else ChatLib.chat(`${GRAY}[${AQUA}TurtleAddons${GRAY}] ${WHITE}Invalid argument. Use "x", "y", or "scale".`);
-  }
-  pogData.save();
+    pogData.save();
 
-  showThing = true;
-  setTimeout(() => showThing = false, 2000);
+    showThing = true;
+    setTimeout(() => showThing = false, 2000)
 }).setName('movepetxp')
