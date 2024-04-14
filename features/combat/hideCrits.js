564a0c60b79c36b3298db2eb572745bfc4e15c20
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-04-14 03:05:37 -0400

    Update hideCrits.js

--- features/combat/hideCrits.js
+++ features/combat/hideCrits.js
@@ -1,8 +1,8 @@
-import settings from "../../settings"
-import { GRAY } from "../../utils/formatting"
+import settings from "../../settings";
+import { GRAY } from "../../utils/formatting";
 
 register("renderEntity", (entity, pos, partialTick, event) => {
-  if (!isNaN(parseInt(entity.getName().replace(/,/g, '').slice(2))) && entity.getName().slice(0, 2) == GRAY && parseInt(entity.getName().replace(/,/g, '').slice(2)) < parseInt(settings.hideNonCrits)) {
-      cancel(event);
-  }
+    if (!isNaN(parseInt(entity.getName().replace(/,/g, '').slice(2))) && entity.getName().slice(0, 2) == GRAY && parseInt(entity.getName().replace(/,/g, '').slice(2)) < parseInt(settings.hideNonCrits)) {
+        cancel(event);
+    }
 })
