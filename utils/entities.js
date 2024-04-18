734a63e57a92f3df233ab793434ff0078a8e59cb
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-04-18 17:32:21 -0400

    Update entities.js

--- utils/entities.js
+++ utils/entities.js
@@ -1,2 +1,3 @@
 export const EntityArmorStand = Java.type('net.minecraft.entity.item.EntityArmorStand').class;
 export const EntityMagmaCube = Java.type("net.minecraft.entity.monster.EntityMagmaCube").class;
+export const EntityEnderDragon = Java.type('net.minecraft.entity.boss.EntityDragon').class;
