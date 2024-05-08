bb46501315a5e9960a3f79e9c363fd1f0de9ed51
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-05-08 19:00:46 -0400

    Update sounds.js

--- utils/sounds.js
+++ utils/sounds.js
@@ -1,2 +1,7 @@
-export const level = new Sound({source: '../modules/TurtleAddons/assets/level.ogg', priority: true});
+export const level1 = new Sound({source: '../modules/TurtleAddons/assets/level1.ogg', priority: true});
+export const level2 = new Sound({source: '../modules/TurtleAddons/assets/level2.ogg', priority: true});
+export const level3 = new Sound({source: '../modules/TurtleAddons/assets/level3.ogg', priority: true});
+export const level4 = new Sound({source: '../modules/TurtleAddons/assets/level4.ogg', priority: true});
+export const level5 = new Sound({source: '../modules/TurtleAddons/assets/level5.ogg', priority: true});
+export const level6 = new Sound({source: '../modules/TurtleAddons/assets/level6.ogg', priority: true});
 export const pling = new Sound({source: '../modules/TurtleAddons/assets/pling.ogg', priority: true});
