0e851527d101a7375721d8e9db89e6a70d4db2dd
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-05-08 18:56:21 -0400

    Update announceEarlyP3.js

--- features/dungeons/announceEarlyP3.js
+++ features/dungeons/announceEarlyP3.js
@@ -2,9 +2,9 @@ import settings from "../../settings";
 
 function inGoldorPhase() {
     if ((Player.getX() > 90 && Player.getX() < 110) && (Player.getY() > 107 && Player.getY() < 145) && (Player.getZ() > 52 && Player.getZ() < 123)) return 1;
-    else if ((Player.getX() > 17 && Player.getX() < 88) && (Player.getY() > 107 && Player.getY() < 145) && (Player.getZ() > 122 && Player.getZ() < 142)) return 2;
-    else if ((Player.getX() > -2 && Player.getX() < 18) && (Player.getY() > 107 && Player.getY() < 145) && (Player.getZ() > 49   && Player.getZ() < 120)) return 3;
-    else if ((Player.getX() > 20 && Player.getX() < 88) && (Player.getY() > 107 && Player.getY() < 145) && (Player.getZ() > 30 && Player.getZ() < 50)) return 4;
+    else if ((Player.getX() > 17 && Player.getX() < 105) && (Player.getY() > 107 && Player.getY() < 145) && (Player.getZ() > 125 && Player.getZ() < 142)) return 2;
+    else if ((Player.getX() > -2 && Player.getX() < 15) && (Player.getY() > 107 && Player.getY() < 145) && (Player.getZ() > 49   && Player.getZ() < 137)) return 3;
+    else if ((Player.getX() > -2 && Player.getX() < 88) && (Player.getY() > 107 && Player.getY() < 145) && (Player.getZ() > 30 && Player.getZ() < 48)) return 4;
     else if ((Player.getX() > 41 && Player.getX() < 68) && (Player.getY() > 110 && Player.getY() < 150) && (Player.getZ() > 59 && Player.getZ() < 117)) return 5;
     else return 0;
 }
