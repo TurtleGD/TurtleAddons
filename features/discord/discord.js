4dc1b5a487c18b2709f7a33a5abbaaafd4bf0cb0
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-05-08 18:55:59 -0400

    Update discord.js

--- features/discord/discord.js
+++ features/discord/discord.js
@@ -13,6 +13,8 @@ function sendWebhook(message) {
     if (settings.webhookPfp == '') webhookPfp = 'https://cdn.discordapp.com/attachments/1070513025959858267/1209659981142691850/discordblue.png?ex=65e7baa5&is=65d545a5&hm=31919c044bbf082e06a37333ceef681ec62577607831c5448011c2ecde866f09&'
     if (settings.webhookMessage == '') webhookMessage = 'Default Message'
 
+    if (settings.webhookTimestamp) webhookMessage += ` (<t:${Math.round(new Date().getTime() / 1000)}:t>)`
+
     request({
         url: settings.webhookLink,
         method: "POST",
