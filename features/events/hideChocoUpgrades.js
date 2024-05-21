deb2355f51dbb753f7f27f2b1875aacf34e31428
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-05-21 15:57:15 -0400

    Add files via upload

---
+++ features/events/hideChocoUpgrades.js
@@ -0,0 +1,5 @@
+﻿import settings from "../../settings";
+
+register('chat', (message, event) => {
+    if (settings.hideChocoUpgrades && (message.includes('has been promoted to') || message.includes('Your Rabbit Barn capacity has been increased to')) && !message.includes(':')) cancel(event);
+}).setCriteria("${message}")
