f2f5d8cae2682f36f8e84b75889778e4ac711796
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-09-05 23:43:43 -0400

    Delete utils directory

--- utils/entities.js
+++
@@ -1,3 +0,0 @@
-export const EntityArmorStand = Java.type('net.minecraft.entity.item.EntityArmorStand').class;
-export const EntityMagmaCube = Java.type("net.minecraft.entity.monster.EntityMagmaCube").class;
-export const EntityEnderDragon = Java.type('net.minecraft.entity.boss.EntityDragon').class;
