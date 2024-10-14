57748e584802e1189fe723afe2fe14f2fe5924d1
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-10-13 22:20:24 -0400

    Add files via upload

---
+++ utils/packets.js
@@ -0,0 +1,3 @@
+export const S32PacketConfirmTranscation = Java.type("net.minecraft.network.play.server.S32PacketConfirmTransaction");
+export const S0FPacketSpawnMob = Java.type("net.minecraft.network.play.server.S0FPacketSpawnMob");
+export const C02PacketUseEntity = Java.type("net.minecraft.network.play.client.C02PacketUseEntity");
---
+++ utils/pogData.js
@@ -0,0 +1,37 @@
+import PogObject from "../../PogData"
+
+export const pogData = new PogObject("TurtleAddons", {
+    triangle1: [],
+    triangle2: [],
+    equals1: [],
+    equals2: [],
+    slash1: [],
+    slash2: [],
+    x1: [],
+    x2: [],
+
+    // ROBPG, json doesn't have infinity smh
+    m7DragonPBs: [69420, 69420, 69420, 69420, 69420],
+
+    minionData: [],
+
+    maskTimerX: 490,
+    maskTimerY: 249,
+    maskTimerScale: 1,
+
+    p5RagTimerX: 490,
+    p5RagTimerY: 249,
+    p5RagTimerScale: 1,
+
+    blazePillarX: 490,
+    blazePillarY: 249,
+    blazePillarScale: 1,
+
+    srbTimerX: 490,
+    srbTimerY: 249,
+    srbTimerScale: 1,
+
+    slayerSplitsX: 490,
+    slayerSplitsY: 249,
+    slayerSplitsScale: 1,
+});
