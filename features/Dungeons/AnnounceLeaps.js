38383a2047a7074a03d7db5df80bfea24dae8e54
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-11-15 01:47:18 -0500

    Delete features directory

--- features/Dungeons/AnnounceLeaps.js
+++
@@ -1,8 +0,0 @@
-import settings from "../../settings";
-
-register("chat", (player, event) => {
-    if (settings.announceLeaps) {
-        ChatLib.command(`pc Leaping to ${player}`);
-        cancel(event);
-    }
-}).setCriteria("You have teleported to ${player}");
--- features/Slayers/Blaze/HideDemonMessages.js
+++
@@ -1,5 +0,0 @@
-import settings from "../../../settings";
-
-register("chat", (a, b, event) => {
-    if (settings.hideDemonMessages) cancel(event)
-}).setCriteria(/You took (.+) true damage from Quazii's beam!|You took (.+) true damage from Typhoeus's fire!/)
