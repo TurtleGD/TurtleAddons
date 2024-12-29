8984bb1c2815420775feea38fca21fa4a8040cf1
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2024-12-28 23:09:25 -0500

    Delete Combat directory

--- Combat/HideNonCrits.js
+++
@@ -1,9 +0,0 @@
-import settings from "../../settings";
-import { GRAY } from "../../utils/formatting";
-import { EntityArmorStand } from "../../utils/entities"
-
-register("renderEntity", (entity, pos, partialTick, event) => {
-    if (entity.getName().slice(0, 2) == GRAY && parseInt(entity.getName().removeFormatting().replace(/,/g, '').slice(2)) < parseInt(settings.hideNonCrits)) {
-        cancel(event);
-    }
-}).setFilteredClass(EntityArmorStand)
