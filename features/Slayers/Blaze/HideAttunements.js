3942939f51be86ae866eb0dd6a9b1821315c3505
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2025-02-06 21:37:59 -0500

    Delete features directory

--- features/Slayers/Blaze/HideAttunements.js
+++
@@ -1,5 +0,0 @@
-import settings from "../../../settings";
-
-register("chat", (message, event) => {
-    if (settings.hideAttunements && (message == "Your hit was reduced by Hellion Shield!" || message.startsWith("Strike using the"))) cancel(event)
-}).setCriteria("${message}")
