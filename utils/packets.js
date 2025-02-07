7089b700a15d6f00fe8f4158b29f8059ab25ad5d
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2025-02-06 21:37:53 -0500

    Delete utils directory

--- utils/packets.js
+++
@@ -1,7 +0,0 @@
-export const S32PacketConfirmTranscation = Java.type("net.minecraft.network.play.server.S32PacketConfirmTransaction");
-export const S0FPacketSpawnMob = Java.type("net.minecraft.network.play.server.S0FPacketSpawnMob");
-export const S2DPacketOpenWindow = Java.type("net.minecraft.network.play.server.S2DPacketOpenWindow");
-export const S2EPacketCloseWindow = Java.type("net.minecraft.network.play.server.S2EPacketCloseWindow");
-
-export const C02PacketUseEntity = Java.type("net.minecraft.network.play.client.C02PacketUseEntity");
-export const C0DPacketCloseWindow = Java.type("net.minecraft.network.play.client.C0DPacketCloseWindow");
