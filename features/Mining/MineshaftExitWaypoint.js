91b0362838346ca967697c91955c0686e0b0ef63
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2025-02-28 01:29:36 -0500

    Update MineshaftExitWaypoint.js

--- features/Mining/MineshaftExitWaypoint.js
+++ features/Mining/MineshaftExitWaypoint.js
@@ -1,37 +1,25 @@
 import settings from "../../settings";
 import { BOLD } from "../../utils/formatting";
-import renderBeaconBeam from "../../../BeaconBeam";
+import { pogData } from "../../utils/pogData";
+import { createWaypoint } from "../../utils/functions";
 
 let exit = [];
 
-// god i need something better than this
 register("worldLoad", () => {
     if (settings.mineshaftExitWaypoint) {
-        Client.scheduleTask(40, () => {
-            if (!Scoreboard.getNames().join("").includes("Glacite Mineshafts")) return;
-            new Thread(() => {
-                for (let x = -10; x < 10; x++) {
-                    for (let y = -5; y < 5; y++) {
-                        for (let z = -10; z <10; z++) {
-                            if (World.getBlockAt(Player.getX() + x, Player.getY() + y, Player.getZ() + z).type.name == "End Portal") {
-                                exit.push(Math.floor(Player.getX() + x), Math.floor(Player.getY() + y), Math.floor(Player.getZ() + z));
-                                return;
-                            }
-                        }
-                    }
-                }
-            }).start();
+        Client.scheduleTask(20, () => {
+            if (pogData.skyblockArea.includes("Glacite Mineshafts")) exit = [Player.getX(), Player.getY(), Player.getZ()];
         })
     }
 })
 
 register("worldUnload", () => {
-    exit.length = 0;
+    exit = [];
 });
 
 register("renderWorld", () => {
     if (settings.mineshaftExitWaypoint && exit[0]) {
         Tessellator.drawString(`${BOLD}Exit`, exit[0] + 1.5, exit[1] + 4, exit[2] + 1.5, Renderer.WHITE, true, 1.5, true);
-        renderBeaconBeam(exit[0] + 1, exit[1] + 4, exit[2] + 1, 1, 1, 1, 0.5, false);
+        createWaypoint(exit[0] + 1, exit[1] + 4, exit[2] + 1, 1, 1, 1, 0.25, 0.5, true);
     }
 });
