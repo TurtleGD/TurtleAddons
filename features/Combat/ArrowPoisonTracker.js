1b7c99951b540050f8788c5a5790767bc5707768
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2025-05-11 04:05:55 -0400

    Update ArrowPoisonTracker.js

--- features/Combat/ArrowPoisonTracker.js
+++ features/Combat/ArrowPoisonTracker.js
@@ -7,20 +7,22 @@ let toxicIcon = new Item("dye").setDamage(10).setStackSize(5);
 let numTwilight = 0;
 let numToxic = 0;
 
-register("tick", () => {
+register("step", () => {
     if (!settings.arrowPoisonTracker) return;
 
+    let inv = Player.getInventory();
+    if (!inv) return;
+    
     numTwilight = 0;
     numToxic = 0;
 
-
-    Player.getInventory().getItems().forEach(item => {
+    inv.getItems().forEach(item => {
         if (item) {
             if (item.getName().removeFormatting().includes("Twilight Arrow Poison")) numTwilight += item.getStackSize();
             else if (item.getName().removeFormatting().includes("Toxic Arrow Poison")) numToxic += item.getStackSize();
         }
     })
-})
+}).setFps(3)
 
 export function drawArrowPoisonTracker() {
     twilightIcon.draw(pogData.arrowPoisonTrackerX, pogData.arrowPoisonTrackerY, pogData.arrowPoisonTrackerScale)
