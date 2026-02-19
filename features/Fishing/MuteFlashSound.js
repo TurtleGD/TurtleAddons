744a95c7a4e3bf7d2529d4eac39e919b32de15cc
TurtleGD <113803739+TurtleGD@users.noreply.github.com>
2026-02-18 21:57:30 -0500

    Delete features directory

--- features/Fishing/MuteFlashSound.js
+++
@@ -1,5 +0,0 @@
-import settings from "../../settings";
-
-register("soundPlay", (pos, name, vol, pitch, cat, event) => {
-    if (settings.muteFlashSound && vol == 1 && pitch == 3 && Player.getHeldItem()?.getRawNBT()?.includes("Flash")) cancel(event)
-}).setCriteria("mob.guardian.elder.idle")
